8bd0895bd18f6049630792ef6c3839e2bd414ce3
Nathan Bridgewater <nathan@iws.io>
2022-05-15 17:55:20 -0500

    Adding no retries and reducing timout for test runtime.

--- src/db.js
+++ src/db.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require('mongodb')
-const debug = require('debug')('web:lib:db')
 const _ = require('lodash')
 
 let _client = null
@@ -8,7 +7,7 @@ exports.init = async function(uri, options) {
   if (_client == null) {
     const optionsWithDefaults = _.defaults(options, { useUnifiedTopology: true })
     const url = new URL(uri)
-    debug(`Connecting to MongoDb: ${url.host}`)
+    console.log(`Connecting to MongoDb: ${url.host}`)
     _client = new MongoClient(uri, optionsWithDefaults)
     await _client.connect()
     await _client.db('admin').command({ ping: 1 }) // sanity check
@@ -22,7 +21,7 @@ exports.getDb = async function getDb(name) {
 }
 
 exports.closeDb = async function closeDb() {
-  debug('Closing MongoDb connection...')
+  console.log('Closing MongoDb connection...')
   if (_client == null) return
   await _client.close()
 }
--- test/init.coffee
+++ test/init.coffee
@@ -1,3 +1,15 @@
-require('../src').db.init "mongodb://localhost:27017/#{process.env.DB_NAME || 'mongo_repo_test'}", (err) ->
-  return console.error err if err?
+{ init } = require('../src/db')
+
+process.env.DEBUG=''
+
+options = {
+  connectTimeoutMS: 5000,
+  reconnectTries: 0,
+  autoReconnect: false
+}
+
+try
+  await init "mongodb://localhost:27017/#{process.env.DB_NAME || 'mongo_repo_test'}", options
   run()
+catch err
+  console.error err if err?
